44c70e8f0ad53be8f2b7e75620194500978ad576
James Moger <james.moger@gitblit.com>
2015-11-10 07:59:09 -0500

    Update to Gitblit 1.7.0-SNAPSHOT

--- src/main/java/com/gitblit/plugin/hipchat/HipChatReceiveHook.java
+++ src/main/java/com/gitblit/plugin/hipchat/HipChatReceiveHook.java
@@ -311,10 +311,9 @@ public class HipChatReceiveHook extends ReceiveHook {
 
     private List<RevCommit> getCommits(GitblitReceivePack receivePack, String baseId, String tipId) {
     	List<RevCommit> list = new ArrayList<RevCommit>();
-		RevWalk walk = receivePack.getRevWalk();
-		walk.reset();
-		walk.sort(RevSort.TOPO);
-		try {
+		try (RevWalk walk = receivePack.getRevWalk()) {
+			walk.reset();
+			walk.sort(RevSort.TOPO);
 			RevCommit tip = walk.parseCommit(receivePack.getRepository().resolve(tipId));
 			RevCommit base = walk.parseCommit(receivePack.getRepository().resolve(baseId));
 			walk.markStart(tip);
@@ -330,8 +329,6 @@ public class HipChatReceiveHook extends ReceiveHook {
 			// Should never happen, the core receive process would have
 			// identified the missing object earlier before we got control.
 			log.error("failed to get commits", e);
-		} finally {
-			walk.release();
 		}
 		return list;
 	}
